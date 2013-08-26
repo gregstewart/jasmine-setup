479540d028adb1fe3c5ae835d08a523cdee97b20
Greg Stewart <gregs@tcias.co.uk>
2013-08-26 12:34:52 +0200

    Fixed indentation and removed junit style xml report

--- karma.conf.js
+++ karma.conf.js
@@ -2,79 +2,78 @@
 module.exports = function (config) {
   config.set({
 
+    // base path, that will be used to resolve files and exclude
+    basePath : './',
 
-  // base path, that will be used to resolve files and exclude
-  basePath : './',
+    // frameworks to use
+    frameworks: ['jasmine'],
 
-  // frameworks to use
-  frameworks: ['jasmine'],
-
-  // list of files / patterns to load in the browser
-  files : [
-    'lib/jquery-1.9.1.min.js',
-    'helper/SpecHelper.js',
-    {pattern: 'src/*.js'},
-    {pattern: 'spec/**/*.js'}
-  ],
-
-
-  // list of files to exclude
-  exclude : [
-
-  ],
+    // list of files / patterns to load in the browser
+    files : [
+      'lib/jquery-1.9.1.min.js',
+      'helper/SpecHelper.js',
+      {pattern: 'src/*.js'},
+      {pattern: 'spec/**/*.js'}
+    ],
 
 
-  // test results reporter to use
-  // possible values: 'dots', 'progress', 'junit'
-  reporters : ['progress','coverage'],
+    // list of files to exclude
+    exclude : [
 
-  preprocessors : {
-      '**/src/**/*.js': 'coverage'
-  },
-
-  coverageReporter : {
-      type : 'text',
-      dir : 'coverage/'
-  },
-
-  // web server port
-  port : 9876,
+    ],
 
 
-  // cli runner port
-  runnerPort : 9100,
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit'
+    reporters : ['progress','coverage'],
+
+    preprocessors : {
+        '**/src/**/*.js': 'coverage'
+    },
+
+    coverageReporter : {
+        type : 'text',
+        dir : 'coverage/'
+    },
+
+    // web server port
+    port : 9876,
 
 
-  // enable / disable colors in the output (reporters and logs)
-  colors : true,
+    // cli runner port
+    runnerPort : 9100,
 
 
-  // level of logging
-  // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-  logLevel : LOG_INFO,
+    // enable / disable colors in the output (reporters and logs)
+    colors : true,
 
 
-  // enable / disable watching file and executing tests whenever any file changes
-  autoWatch : true,
+    // level of logging
+    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+    logLevel : LOG_INFO,
 
 
-  // Start these browsers, currently available:
-  // - Chrome
-  // - ChromeCanary
-  // - Firefox
-  // - Opera
-  // - Safari (only Mac)
-  // - PhantomJS
-  // - IE (only Windows)
-  browsers : ['PhantomJS'],
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch : true,
 
 
-  // If browser does not capture in given timeout [ms], kill it
-  captureTimeout : 60000,
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers : ['PhantomJS'],
 
 
-  // Continuous Integration mode
-  // if true, it capture browsers, run tests and exit
-  singleRun : false
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout : 60000,
+
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun : false
   });
 };
