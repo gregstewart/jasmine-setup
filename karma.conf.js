bc11b8e22b894a36dc4f22103b505339fa59bc72
Greg Stewart <gregs@tcias.co.uk>
2013-08-26 12:28:51 +0200

    Updated karma config to use the new configuration style

--- karma.conf.js
+++ karma.conf.js
@@ -1,77 +1,80 @@
 // Karma configuration
-// Generated on Tue Mar 19 2013 10:33:15 GMT+0000 (GMT Standard Time)
+module.exports = function (config) {
+  config.set({
 
 
-// base path, that will be used to resolve files and exclude
-basePath = './';
+  // base path, that will be used to resolve files and exclude
+  basePath : './',
+
+  // frameworks to use
+  frameworks: ['jasmine'],
+
+  // list of files / patterns to load in the browser
+  files : [
+    'lib/jquery-1.9.1.min.js',
+    'helper/SpecHelper.js',
+    {pattern: 'src/*.js'},
+    {pattern: 'spec/**/*.js'}
+  ],
 
 
-// list of files / patterns to load in the browser
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'lib/jquery-1.9.1.min.js',
-  'helper/SpecHelper.js',
-  'spec/**/*.js',
-  'src/**/*.js'
-];
+  // list of files to exclude
+  exclude : [
+
+  ],
 
 
-// list of files to exclude
-exclude = [
+  // test results reporter to use
+  // possible values: 'dots', 'progress', 'junit'
+  reporters : ['progress','coverage'],
 
-];
+  preprocessors : {
+      '**/src/**/*.js': 'coverage'
+  },
+
+  coverageReporter : {
+      type : 'text',
+      dir : 'coverage/'
+  },
+
+  // web server port
+  port : 9876,
 
 
-// test results reporter to use
-// possible values: 'dots', 'progress', 'junit'
-reporters = ['progress','coverage','junit'];
+  // cli runner port
+  runnerPort : 9100,
 
-preprocessors = {
-    '**/src/**/*.js': 'coverage'
+
+  // enable / disable colors in the output (reporters and logs)
+  colors : true,
+
+
+  // level of logging
+  // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+  logLevel : LOG_INFO,
+
+
+  // enable / disable watching file and executing tests whenever any file changes
+  autoWatch : true,
+
+
+  // Start these browsers, currently available:
+  // - Chrome
+  // - ChromeCanary
+  // - Firefox
+  // - Opera
+  // - Safari (only Mac)
+  // - PhantomJS
+  // - IE (only Windows)
+  browsers : ['PhantomJS'],
+
+
+  // If browser does not capture in given timeout [ms], kill it
+  captureTimeout : 60000,
+
+
+  // Continuous Integration mode
+  // if true, it capture browsers, run tests and exit
+  singleRun : false
+  });
 };
-
-coverageReporter = {
-    type : 'text',
-    dir : 'coverage/'
-}
-
-// web server port
-port = 9876;
-
-
-// cli runner port
-runnerPort = 9100;
-
-
-// enable / disable colors in the output (reporters and logs)
-colors = true;
-
-
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
-
-
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = true;
-
-
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['PhantomJS'];
-
-
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 60000;
-
-
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
