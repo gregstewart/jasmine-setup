499b80869d5499d031e6939bd778428643e64fcd
Greg Stewart <gregs@tcias.co.uk>
2014-05-07 12:04:16 +0100

    Removing the SpecRunner.html file. Moving to grunt to run karma and added a linting watch task as well. Update karma version and configuration. Coverage reporting uses a mix of text and html output

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+module.exports = function(grunt) {
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      all: ['Gruntfile.js', 'src/js/**/*.js', 'spec/**/*.js']
+    },
+    karma: {
+      unit: {
+        configFile: 'karma.conf.js'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-karma');
+
+  grunt.registerTask('default', ['jshint', 'karma']);
+};
--- karma.conf.js
+++ karma.conf.js
@@ -3,58 +3,67 @@ module.exports = function (config) {
   config.set({
 
     // base path, that will be used to resolve files and exclude
-    basePath : './',
+    basePath: './',
 
     // frameworks to use
     frameworks: ['jasmine'],
 
+    plugins: [
+      'karma-jasmine',
+      'karma-coverage',
+      'karma-phantomjs-launcher'
+    ],
+
     // list of files / patterns to load in the browser
-    files : [
+    files: [
       'lib/jquery-1.9.1.min.js',
       'helper/SpecHelper.js',
-      {pattern: 'src/*.js'},
+      {pattern: 'src/**/*.js'},
       {pattern: 'spec/**/*.js'}
     ],
 
 
     // list of files to exclude
-    exclude : [
+    exclude: [
 
     ],
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress', 'junit'
-    reporters : ['progress','coverage'],
+    reporters: ['progress', 'coverage'],
 
-    preprocessors : {
-        '**/src/**/*.js': 'coverage'
+    preprocessors: {
+      '**/src/**/*.js': 'coverage'
     },
 
-    coverageReporter : {
-        type : 'text',
-        dir : 'coverage/'
+    coverageReporter: {
+      reporters: [
+        { type: 'html'},
+        { type: 'text-summary' }
+      ],
+      dir: 'coverage/'
     },
 
     // web server port
-    port : 9876,
+    port: 9876,
 
 
     // cli runner port
-    runnerPort : 9100,
+    runnerPort: 9100,
 
 
     // enable / disable colors in the output (reporters and logs)
-    colors : true,
+    colors: true,
 
 
     // level of logging
     // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-    logLevel : LOG_INFO,
+    logLevel: config.LOG_INFO,
 
 
     // enable / disable watching file and executing tests whenever any file changes
-    autoWatch : true,
+    autoWatch: true,
 
 
     // Start these browsers, currently available:
@@ -65,15 +74,15 @@ module.exports = function (config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers : ['PhantomJS'],
+    browsers: ['PhantomJS'],
 
 
     // If browser does not capture in given timeout [ms], kill it
-    captureTimeout : 60000,
+    captureTimeout: 60000,
 
 
     // Continuous Integration mode
     // if true, it capture browsers, run tests and exit
-    singleRun : false
+    singleRun: false
   });
 };
